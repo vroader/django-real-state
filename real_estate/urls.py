f127392ba5409429863e240be86861c8d5cda7b0
vroader <vroader@gmail.com>
2025-01-14 23:00:04 -0300

    feat: add custom user model

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -8,3 +8,6 @@ urlpatterns = [
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to Real Estate Portal"
