594937a87101eef7de8a9f6a99cd0c13af74320f
vroader <vroader@gmail.com>
2025-05-01 16:00:36 -0300

    chore: figure pytest and coverage

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -18,13 +18,13 @@ class Gender(models.TextChoices):
 class Profile(TimeStampedUUIDModel):
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     phone_number = PhoneNumberField(
-        verbose_name=_("Phone Number"), max_length=30, default="+5561999999999"
+        verbose_name=_("Phone Number"), max_length=30, default="+41524204242"
     )
     about_me = models.TextField(
-        verbose_name=_("About Me"), default="Diga algo sobre você"
+        verbose_name=_("About me"), default="say something about yourself"
     )
     license = models.CharField(
-        verbose_name=_("Número do CRECI"), max_length=20, blank=True, null=True
+        verbose_name=_("Real Estate license"), max_length=20, blank=True, null=True
     )
     profile_photo = models.ImageField(
         verbose_name=_("Profile Photo"), default="/profile_default.png"
@@ -36,35 +36,33 @@ class Profile(TimeStampedUUIDModel):
         max_length=20,
     )
     country = CountryField(
-        verbose_name=_("Country"), default="BR", blank=False, null=False
+        verbose_name=_("Country"), default="KE", blank=False, null=False
     )
     city = models.CharField(
-        verbose_name=_("Cidade"),
-        max_length=18,
-        default="Brasília",
+        verbose_name=_("City"),
+        max_length=180,
+        default="Nairobi",
         blank=False,
         null=False,
     )
     is_buyer = models.BooleanField(
-        verbose_name=_("Comprador"),
+        verbose_name=_("Buyer"),
         default=False,
-        help_text=_("Você está procurando comprar um imóvel?"),
+        help_text=_("Are you looking to Buy a Property?"),
     )
     is_seller = models.BooleanField(
-        verbose_name=_("Vendedor"),
+        verbose_name=_("Seller"),
         default=False,
-        help_text=_("Você está procurando vender um imóvel?"),
+        help_text=_("Are you looking to sell a property?"),
     )
     is_agent = models.BooleanField(
-        verbose_name=_("Corretor"),
-        default=False,
-        help_text=_("Você é um corretor de imóveis?"),
+        verbose_name=_("Agent"), default=False, help_text=_("Are you an agent?")
     )
-    top_agent = models.BooleanField(verbose_name=_("Corretor Ranqueado"), default=False)
-    rating = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    top_agent = models.BooleanField(verbose_name=_("Top Agent"), default=False)
+    rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     num_reviews = models.IntegerField(
-        verbose_name=_("Número de Avaliações"), default=0, null=True, blank=True
+        verbose_name=_("Number of Reviews"), default=0, null=True, blank=True
     )
 
     def __str__(self):
-        return f"Perfil {self.user.username}"
+        return f"{self.user.username}'s profile"
